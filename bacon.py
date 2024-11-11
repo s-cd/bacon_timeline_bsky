19b007e4a7534c840ea86c20fc905a4cba0383d0
s-cd <50283939+s-cd@users.noreply.github.com>
2024-11-11 07:46:08 -0700

    should work
    it should work but i cannot test it due to timeout req, sorry animeavi for replacing ur ruby with python

---
+++ bacon.py
@@ -0,0 +1,17 @@
+from atproto import Client
+import time
+client = Client()
+client.login('username', 'password')
+
+with open("text.txt", "r") as file:
+    text = file.readlines()
+
+i = 0
+for t in text:
+    filename = "pics/bacon" + str(i) +'.png'
+    with open(filename, 'rb') as f:
+        img_data = f.read()
+    client.send_image(text=t.strip(), image=img_data, image_alt="bacon")
+    print(t.strip())
+    i += 1
+    time.sleep(1000)
